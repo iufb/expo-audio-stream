0fd35fa33a18f24c4979aa6db31cd2c1324b251a
iufb <bashirov3ld2@gmail.com>
2026-07-22 13:52:06 +0500

    Migrate to SDK 57 and npm

--- app.plugin.js
+++ app.plugin.js
@@ -1,1 +1,40 @@
-module.exports = require('./plugin/build')
+const { AndroidConfig, withAndroidManifest, withInfoPlist } = require('@expo/config-plugins');
+
+const MICROPHONE_USAGE = 'Allow $(PRODUCT_NAME) to access your microphone';
+
+function withRecordingPermission(config) {
+  config = withInfoPlist(config, (config) => {
+    config.modResults.NSMicrophoneUsageDescription =
+      config.modResults.NSMicrophoneUsageDescription || MICROPHONE_USAGE;
+
+    const existingBackgroundModes = config.modResults.UIBackgroundModes || [];
+    if (!existingBackgroundModes.includes('audio')) {
+      existingBackgroundModes.push('audio');
+    }
+    config.modResults.UIBackgroundModes = existingBackgroundModes;
+
+    return config;
+  });
+
+  config = withAndroidManifest(config, (config) => {
+    const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(config.modResults);
+
+    AndroidConfig.Manifest.addMetaDataItemToMainApplication(
+      mainApplication,
+      'android.permission.RECORD_AUDIO',
+      MICROPHONE_USAGE
+    );
+
+    AndroidConfig.Manifest.addMetaDataItemToMainApplication(
+      mainApplication,
+      'android.permission.FOREGROUND_SERVICE',
+      'This apps needs access to the foreground service to record audio in the background'
+    );
+
+    return config;
+  });
+
+  return config;
+}
+
+module.exports = withRecordingPermission;
